138c26cd8cb5283062906c07436360fc9d65d3a6
abhipandey11 <abhi.its2004bhopal@gmail.com>
2025-12-23 01:03:40 +0530

    solved leetcode no 184 mysql

---
+++ Leetcode/SQL/S184.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT d.name as Department,e.name as Employee,e.salary as Salary 
+FROM Employee e JOIN Department d ON e.departmentId=d.id 
+WHERE (e.departmentId,e.Salary) IN(SELECT departmentId,MAX(Salary) FROM Employee GROUP BY departmentId);
