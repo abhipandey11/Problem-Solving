a784338001db6526cc2641647fe8f295f4c2557e
abhipandey11 <abhi.its2004bhopal@gmail.com>
2025-12-23 01:38:17 +0530

    nother variation in 184 leetcode question sql

--- Leetcode/SQL/S184.sql
+++ Leetcode/SQL/S184.sql
@@ -2,3 +2,27 @@
 SELECT d.name as Department,e.name as Employee,e.salary as Salary 
 FROM Employee e JOIN Department d ON e.departmentId=d.id 
 WHERE (e.departmentId,e.Salary) IN(SELECT departmentId,MAX(Salary) FROM Employee GROUP BY departmentId);
+---------------------------------------------------------------------------------------------------------
+
+# Write your MySQL query statement below
+WITH rnk_table AS(
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        RANK() OVER ( 
+            PARTITION BY d.name
+            ORDER BY e.salary DESC) AS rnk
+    FROM 
+        Employee e
+    LEFT JOIN 
+        Department d
+        ON e.departmentId = d.id
+)
+
+SELECT
+    Department, 
+    Employee, 
+    Salary
+FROM rnk_table
+WHERE rnk = 1
