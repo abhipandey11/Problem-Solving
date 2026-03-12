4a2095b354f73bba0690ca8d033fcd51839c8c6d
abhipandey11 <abhi.its2004bhopal@gmail.com>
2026-03-13 02:00:38 +0530

    solved linked list cycle problem

---
+++ Leetcode/DSA/141DSA.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
+
+//iteration approach
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> seen=new HashSet<>();
+        ListNode curr=head;
+        while(curr!=null){
+            if(seen.contains(curr)){
+                return true;
+            }
+            seen.add(curr);
+            curr=curr.next;
+        }
+        return false;
+        
+    }
+}
